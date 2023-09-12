751f187b1839116a689f466946529f6818726656
przemek06 <swiatprzemyslaw@gmail.com>
2023-09-12 11:52:58 +0200

    backend init

---
+++ iot_mobile_backend/src/main/java/edu/pwr/iotmobile/IotMobileBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.pwr.iotmobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotMobileBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IotMobileBackendApplication.class, args);
+    }
+
+}
---
+++ iot_mobile_backend/src/test/java/edu/pwr/iotmobile/IotMobileBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pwr.iotmobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotMobileBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
