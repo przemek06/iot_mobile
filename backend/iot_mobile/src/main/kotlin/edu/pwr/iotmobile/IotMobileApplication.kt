86b12760da6610e828266b94e817dc8a4edb251b
przemek06 <swiatprzemyslaw@gmail.com>
2023-09-18 17:21:43 +0200

    basic backend in kotlin

---
+++ backend/iot_mobile/src/main/kotlin/edu/pwr/iotmobile/IotMobileApplication.kt
@@ -0,0 +1,11 @@
+package edu.pwr.iotmobile
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IotMobileApplication
+
+fun main(args: Array<String>) {
+	runApplication<IotMobileApplication>(*args)
+}
