ea623c745eead6e91ee5d78188b59a663b5640ca
agata <agata.piasecka@outlook.com>
2023-10-03 14:22:55 +0200

    added activate screen

---
+++ frontend/app/src/main/java/edu/pwr/iotmobile/androidimcs/ui/screens/loginregister/activate/ActivateAccountScreen.kt
@@ -0,0 +1,13 @@
+package edu.pwr.iotmobile.androidimcs.ui.screens.loginregister.activate
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ActivateAccountScreen() {
+
+}
+
+@Composable
+private fun ActivateAccountScreenContent() {
+
+}
