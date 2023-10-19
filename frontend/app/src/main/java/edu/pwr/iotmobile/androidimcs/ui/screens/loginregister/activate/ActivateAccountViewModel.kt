e4a7e6e06565e0b98c2fcf71e8714adde719eda7
agata <agata.piasecka@outlook.com>
2023-10-19 21:23:43 +0200

    activate account api integration viewmodel

--- frontend/app/src/main/java/edu/pwr/iotmobile/androidimcs/ui/screens/loginregister/activate/ActivateAccountViewModel.kt
+++ frontend/app/src/main/java/edu/pwr/iotmobile/androidimcs/ui/screens/loginregister/activate/ActivateAccountViewModel.kt
@@ -1,11 +1,21 @@
 package edu.pwr.iotmobile.androidimcs.ui.screens.loginregister.activate
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import edu.pwr.iotmobile.androidimcs.data.result.ActivateAccountResult
+import edu.pwr.iotmobile.androidimcs.helpers.event.Event
+import edu.pwr.iotmobile.androidimcs.helpers.toast.Toast
+import edu.pwr.iotmobile.androidimcs.model.repository.UserRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
-class ActivateAccountViewModel() : ViewModel() {
+class ActivateAccountViewModel(
+    private val repository: UserRepository,
+    val event: Event,
+    val toast: Toast
+) : ViewModel() {
 
     private val _uiState = MutableStateFlow(ActivateAccountUiState.default())
     val uiState = _uiState.asStateFlow()
@@ -18,10 +28,25 @@ class ActivateAccountViewModel() : ViewModel() {
     }
 
     fun onActivate() {
-        // TODO
+        viewModelScope.launch {
+            val code = _uiState.value.inputField
+            val result = repository.verifyUser(code.text)
+            when (result) {
+                ActivateAccountResult.Success -> event.event(ACTIVATE_ACCOUNT_SUCCESS_EVENT)
+                ActivateAccountResult.IncorrectCode -> _uiState.update {
+                    val updatedInputField = code.copy(isError = true)
+                    it.copy(inputField = updatedInputField)
+                }
+                ActivateAccountResult.Failure -> toast.toast("Error - could not activate account.")
+            }
+        }
     }
 
     fun onResendCode() {
         // TODO: send email + show toast message
+    }
+
+    companion object {
+        const val ACTIVATE_ACCOUNT_SUCCESS_EVENT = "activate_account_success"
     }
 }
