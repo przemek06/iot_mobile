7e59d8c973f42645704a8dc93ea65f7ffdc7e455
agata <agata.piasecka@outlook.com>
2023-10-26 19:58:44 +0200

    retrofit logs

--- frontend/app/src/main/java/edu/pwr/iotmobile/androidimcs/app/retrofit/AppRetrofit.kt
+++ frontend/app/src/main/java/edu/pwr/iotmobile/androidimcs/app/retrofit/AppRetrofit.kt
@@ -1,14 +1,21 @@
 package edu.pwr.iotmobile.androidimcs.app.retrofit
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object AppRetrofit {
-    private const val BASE_URL = "http://172.20.10.8:8080/"
+    private const val BASE_URL = "http://192.168.66.47:8080/"
 
     fun create(): Retrofit {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
         return Retrofit
             .Builder()
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .baseUrl(BASE_URL)
             .build()
