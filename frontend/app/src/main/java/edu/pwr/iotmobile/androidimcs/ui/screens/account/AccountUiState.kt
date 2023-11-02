da1d5ecb2f20f7497e8d6d990b7c211200a21177
Szymon Misztal <technosudo@softron.pl>
2023-11-02 13:26:45 +0100

    account api

--- frontend/app/src/main/java/edu/pwr/iotmobile/androidimcs/ui/screens/account/AccountUiState.kt
+++ frontend/app/src/main/java/edu/pwr/iotmobile/androidimcs/ui/screens/account/AccountUiState.kt
@@ -4,10 +4,11 @@ import edu.pwr.iotmobile.androidimcs.R
 import edu.pwr.iotmobile.androidimcs.data.InputFieldData
 import edu.pwr.iotmobile.androidimcs.data.MenuOption
 import edu.pwr.iotmobile.androidimcs.data.StatData
+import edu.pwr.iotmobile.androidimcs.data.User
+import edu.pwr.iotmobile.androidimcs.data.UserRole
 
 data class AccountUiState(
-    val displayName: String,
-    val email: String,
+    val user: User,
     val changePasswordOption: MenuOption,
     val inputField: InputFieldData,
     val statList: List<StatData>,
@@ -18,20 +19,26 @@ data class AccountUiState(
             displayName: String = "display name",
             email: String = "email",
             changePasswordOption: MenuOption = MenuOption(titleId = R.string.change_password),
-            inputField: InputFieldData = InputFieldData(label = R.string.display_name),
-            statList: List<StatData> = listOf(
-                StatData(label = R.string.dashboards_total),
-                StatData(label = R.string.accessed_dashboards),
-                StatData(label = R.string.created_topics_groups)
+            inputField: InputFieldData = InputFieldData(
+                text = "",
+                label = R.string.name,
+                errorMessage = R.string.not_empty,
+                isError = false
             ),
-            options: List<MenuOption> = emptyList()
+            statList: List<StatData> = emptyList(),
+            options: List<MenuOption> = emptyList(),
+            user: User = User(
+                id = 0,
+                displayName = "",
+                email = "",
+                role = UserRole.Normal
+            )
         ) = AccountUiState(
-            displayName = displayName,
-            email = email,
             changePasswordOption = changePasswordOption,
             inputField = inputField,
             statList = statList,
-            options = options
+            options = options,
+            user = user
         )
     }
 }
