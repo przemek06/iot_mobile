69ac6dc115fedc4cca16efd5a274b66391dcb7cb
agata <agata.piasecka@outlook.com>
2023-11-11 15:16:06 +0100

    fix

--- backend/iot_mobile/src/main/kotlin/edu/pwr/iotmobile/websocket/ProjectDeletedWebSocketHandler.kt
+++ backend/iot_mobile/src/main/kotlin/edu/pwr/iotmobile/websocket/ProjectDeletedWebSocketHandler.kt
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import edu.pwr.iotmobile.error.exception.NoAuthenticationException
 import edu.pwr.iotmobile.error.exception.NotAllowedException
 import edu.pwr.iotmobile.service.*
+import org.springframework.stereotype.Component
 import org.springframework.web.socket.CloseStatus
 import org.springframework.web.socket.TextMessage
 import org.springframework.web.socket.WebSocketSession
 import org.springframework.web.socket.handler.TextWebSocketHandler
 import reactor.core.Disposable
 
+@Component
 class ProjectDeletedWebSocketHandler(
         private val projectDeletedNotificationService: ProjectDeletedNotificationService,
         val userService: UserService,
