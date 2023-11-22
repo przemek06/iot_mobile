a0077feb48286150e5c9d6975bdd30c69ae1ebe7
przemek06 <swiatprzemyslaw@gmail.com>
2023-11-23 00:23:06 +0100

    fix redirect

--- backend/iot_mobile/src/main/kotlin/edu/pwr/iotmobile/controller/IntegrationController.kt
+++ backend/iot_mobile/src/main/kotlin/edu/pwr/iotmobile/controller/IntegrationController.kt
@@ -3,6 +3,7 @@ package edu.pwr.iotmobile.controller
 import edu.pwr.iotmobile.dto.DiscordChannelDTO
 import edu.pwr.iotmobile.dto.UriDTO
 import edu.pwr.iotmobile.service.IntegrationService
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.ModelAndView
+import org.springframework.web.servlet.view.RedirectView
 import java.net.URI
 
 
@@ -31,8 +34,19 @@ class IntegrationController(val integrationService: IntegrationService) {
     }
 
     @GetMapping("/anon/discord")
-    fun discordCallback(@RequestParam("guild_id") guildId: String): String {
-        return "redirect:$redirectUri?type=discord&id=$guildId"
+    fun discordCallback(@RequestParam("guild_id") guildId: String) : String {
+        println("$redirectUri?type=discord&id=$guildId")
+        return "<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+                "    <title>Return to Application</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "    <p>Click to return to the application: <a href=\"$redirectUri?type=discord&id=$guildId\">Return Link</a></p>\n" +
+                "</body>\n" +
+                "</html>"
     }
 
     @GetMapping("/user/discord/channels/{guildId}")
