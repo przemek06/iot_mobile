962319a15da3b465e7e713c0f2b84a5ad0973a66
dzasta997 <justynaturowska97@gmail.com>
2023-11-26 19:25:17 +0100

    change logger names

--- backend/iot_mobile/src/main/kotlin/edu/pwr/iotmobile/integration/TelegramIntegrationAction.kt
+++ backend/iot_mobile/src/main/kotlin/edu/pwr/iotmobile/integration/TelegramIntegrationAction.kt
@@ -1,6 +1,5 @@
 package edu.pwr.iotmobile.integration
 
-import edu.pwr.iotmobile.error.exception.SlackException
 import edu.pwr.iotmobile.error.exception.TelegramException
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -10,7 +9,7 @@ class TelegramIntegrationAction(
     private val telegramBot: TelegramBot
 ) : IntegrationAction {
 
-    private val logger: Logger = LoggerFactory.getLogger("EmailIntegrationAction")
+    private val logger: Logger = LoggerFactory.getLogger("TelegramIntegrationAction")
 
     override fun performAction(data: String) {
         try {
