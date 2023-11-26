63f5677e3f6a06ba90ed82539c88841b896c31bb
agata <agata.piasecka@outlook.com>
2023-11-27 00:45:35 +0100

    on dragged component color change

--- frontend/app/src/main/java/edu/pwr/iotmobile/androidimcs/ui/screens/dashboard/DashboardComponentsList.kt
+++ frontend/app/src/main/java/edu/pwr/iotmobile/androidimcs/ui/screens/dashboard/DashboardComponentsList.kt
@@ -2,6 +2,7 @@
 
 package edu.pwr.iotmobile.androidimcs.ui.screens.dashboard
 
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.gestures.animateScrollBy
@@ -33,6 +34,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.PointerInputChange
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onGloballyPositioned
@@ -204,6 +206,11 @@ fun LazyStaggeredGridItemScope.ComponentWrapper(
 
     val zIndex = if (isDragged) 3f else 1f
 
+    val bgColor: Color by animateColorAsState(
+        if (isDragged) MaterialTheme.colorScheme.secondaryContainer
+        else MaterialTheme.colorScheme.background
+    )
+
     LaunchedEffect(interactionSource) {
         interactionSource.interactions.collect { interaction ->
             when (interaction) {
@@ -274,7 +281,7 @@ fun LazyStaggeredGridItemScope.ComponentWrapper(
                 .clip(CardDefaults.shape),
             border = BorderStroke(width = 2.dp, color = MaterialTheme.colorScheme.primary),
             colors = CardDefaults.cardColors(
-                containerColor = MaterialTheme.colorScheme.background,
+                containerColor = bgColor,
             )
         ) {
             Column(
