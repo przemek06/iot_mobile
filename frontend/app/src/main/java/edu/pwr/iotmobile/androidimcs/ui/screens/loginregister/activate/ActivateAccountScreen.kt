40595dd9cd6c055f61c04a53ca7ff7a95ba8cb41
agata <agata.piasecka@outlook.com>
2023-12-10 19:16:05 +0100

    activate code init fix

--- frontend/app/src/main/java/edu/pwr/iotmobile/androidimcs/ui/screens/loginregister/activate/ActivateAccountScreen.kt
+++ frontend/app/src/main/java/edu/pwr/iotmobile/androidimcs/ui/screens/loginregister/activate/ActivateAccountScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
@@ -44,6 +45,10 @@ fun ActivateAccountScreen(navigation: ActivateAccountNavigation) {
     val viewModel = koinViewModel<ActivateAccountViewModel>()
     val uiState by viewModel.uiState.collectAsState()
     val uiInteraction = ActivateAccountUiInteraction.default(viewModel)
+
+    LaunchedEffect(Unit) {
+        viewModel.init(navigation.email)
+    }
 
     val context = LocalContext.current
     viewModel.event.CollectEvent(context) {
