c94e63024f20efdfa09e1ee7e647f37c37ff98e6
agata <agata.piasecka@outlook.com>
2023-12-14 01:06:40 +0100

    view model tests

---
+++ frontend/app/src/androidTest/java/edu/pwr/iotmobile/androidimcs/AddComponentUITest.kt
@@ -0,0 +1,15 @@
+package edu.pwr.iotmobile.androidimcs
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class AddComponentUITest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+
+}
